de7bb72eab592ba072979067486f6b3f40943a09
amitkulkarni0391 <amitkulkarni87654@gmai.com>
2024-06-15 15:41:44 +0530

    Added changes as per review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
 	int i = 10;
+	
+	By locator = By.id("cart");
+	
+	public void addCart() {
+		System.out.println("Add cart");
+	}
 
 }
